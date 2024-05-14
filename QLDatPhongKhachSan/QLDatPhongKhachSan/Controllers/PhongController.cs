1ce19d65f25c5233a9720949b974605691e2bd81
huyphamnq <huyphamnq.vn@gmail.com>
2024-05-15 01:19:06 +0700

    cap nhat

--- QLDatPhongKhachSan/QLDatPhongKhachSan/Controllers/PhongController.cs
+++ QLDatPhongKhachSan/QLDatPhongKhachSan/Controllers/PhongController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using QLDatPhongKhachSan.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,5 +20,22 @@ namespace QLDatPhongKhachSan.Controllers
         }
 
         //code o day
+        [HttpPost]
+        public async Task<IActionResult> AddDatPhong(string MaDatPhong, string MaKH, string MaPhong, string MaNV, DateTime NgayDat, DateTime NgayNhanPhong, DateTime NgayTraPhong, bool DaThanhToan)
+        {
+            try
+            {
+                await _context.Database.ExecuteSqlInterpolatedAsync(
+                    $"Exec spAPI_Add_DatPhong {MaDatPhong}, {MaKH}, {MaPhong}, {MaNV}, {NgayDat}, {NgayNhanPhong}, {NgayTraPhong}, {DaThanhToan}"
+                );
+                return Ok("Đặt phòng thành công");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Đã xảy ra lỗi khi đặt phòng: " + ex.Message);
+            }
+        }
+
+
     }
 }
